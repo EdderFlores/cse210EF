49003722cffa7984f6d724f88cb560c38932bdb1
EdderFlores <floresmoy11@gmail.com>
2023-09-22 00:27:42 -0600

    Prep 3 class completed for magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
+using System.Reflection.PortableExecutable;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = new Random().Next(1,101);
+
+        bool guessedCorrectly = false;
+        while (!guessedCorrectly)
+        {
+            Console.WriteLine("What is your guess?");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
+                guessedCorrectly = true;
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lowe");
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
